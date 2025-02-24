9ea1ca79b6fcea64942f162d8946326862bd5c80
leandro509 <leandrogabriel509@gmail.com>
2025-02-24 15:52:16 -0300

    correcao matrizes

--- Matrizes/MatrizExe03.java
+++ Matrizes/MatrizExe03.java
@@ -7,19 +7,21 @@ public class MatrizExe03 {
         System.out.println("Quantas pessoas vao ser entrevistadas : ");
         int qtdPessoas = scan.nextInt();
         int qtdFilhos = 0;
+        //Matriz com apenas linhas
         String[][] nomeFilhos = new String[qtdPessoas][];
 
         for (int i = 0; i < qtdPessoas; i++) {
 
-            System.out.println("Quantos filhos voce tem : ");
+            System.out.println("Quantos filhos a pessoa " + (i+1) + " tem : ");
             qtdFilhos = scan.nextInt();
             scan.nextLine();
 
+            //Matriz adiciona na linha atual a quantidade de colunas
             nomeFilhos[i] = new String[qtdFilhos];
 
             for (int j = 0; j < qtdFilhos ; j++) {
 
-                System.out.println("Qual o nome do seu filho " + (j + 1) + " : ");
+                System.out.println("Qual o nome do filho " + (j + 1) + " : ");
                 nomeFilhos[i][j] = scan.nextLine();
                 
 
@@ -31,10 +33,11 @@ public class MatrizExe03 {
 
             System.out.println("Pessoa " + (i+1) + " - " + (nomeFilhos[i].length) + " filhos");
 
+            //nomeFilhos[i].length - Contando quantas colunas tem na linha
             for(int j = 0; j < nomeFilhos[i].length; j++) {
 
                
-                System.out.print(nomeFilhos[i][j] + "  " );
+                System.out.println(nomeFilhos[i][j]);
                 
                 
 
